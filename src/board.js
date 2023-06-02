fca6bcc7d19c41c365c878e09bc75392d150dc79
J05nick <joschabat@outlook.com>
2023-06-02 18:16:26 -0500

    Board logic initilization

---
+++ src/board.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./board.css";
+
+function Board(){
+  return (
+    //<img src={process.env.PUBLIC_URL + 'chessboard.png'} alt="chessboard"></img>
+    <table className="BoardStyle">
+      <tr>
+        <td ></td>
+      </tr>
+
+    </table>
+  )
+}
+
+export default Board;
